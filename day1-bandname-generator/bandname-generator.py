891dda1497b60375b9fdc59f51676e418d8a4518
Dominique <dominiquejacqueline@yahoo.com>
2025-02-12 15:24:51 -0500

    Added day 1 project folder

---
+++ day1-bandname-generator/bandname-generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator!")
+user_name = input("What is your name?\n")
+print("Hello " + user_name + "!")
+origin_city = input("What city did you grow up in?\n")
+pet_name = input("What is the name of your pet?\n")
+band_name = origin_city + " " +  pet_name
+print("Your band name could be: " + band_name + "!")
