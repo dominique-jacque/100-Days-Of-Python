a7e63714d6e5c8a1f4f4a0a9cf9ca1e9e15e1cbc
Dominique <dominiquejacqueline@yahoo.com>
2025-02-17 15:13:44 -0500

    Added day 4 exercise and rockpaperscissor python file and Updated exercises script

---
+++ day4-rockpaperscissors/exercises.py
@@ -0,0 +1,13 @@
+import random
+
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+# random_number_0_to_1 = random.random() * 10
+# print(random_number_0_to_1)
+
+# random_float = random.uniform(1, 10) # this works the same as the random function but may include the upper bound
+# print(random_float)
+
+# Exercise 1
+print("Heads") # even
+print("Tails") # odd
